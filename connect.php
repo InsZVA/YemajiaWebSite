8d5e92d37e8d8bec4b6553b09bb0276380c99fb3
InsZVA <inszva@126.com>
2015-05-24 15:47:01 +0800

    Delete connect.php
    avoid attck

--- connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-/* Code By InsZVA , Reuse Please Indicate The Source */
-/* www.lizile.net */
-	$mysql_address='qdm158444573.my3w.com'; //The Address Of MySQL
-    $mysql_username='qdm158444573'; //The User Of MySQL
-    $mysql_password='yechenzongjingli'; //As the Name Says
-	$mysql_db='qdm158444573_db';
-?>
